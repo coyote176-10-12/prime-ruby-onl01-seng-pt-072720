3a6fba092e338f9f8df8d51a947ca641acbc2fc7
Javier Aguilar <coyote176@msn.com>
2020-08-07 21:45:22 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,11 @@
-# Add  code here!
+#def prime?(number)
+ # (2..number/2).none? {|n| return false if number <= 1 || number % n == 0}
+#end
+def prime?(value)
+  if value <= 1 || value == 0 || value == 1
+    return false
+  elsif
+    (2..value - 1).each do |i|
+      if value % i == 0
+        return false
+      end
